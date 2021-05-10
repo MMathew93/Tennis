f46debbf7cc20a794830cee2a2b89a2369d1ef7a
lyndexer <mckinney.m93@gmail.com>
2021-05-10 16:48:59 -0700

    added some movement to the left paddle, however having issues trying to seperate classes and cut down repition of code

--- src/objects/Paddle.js
+++ src/objects/Paddle.js
@@ -5,7 +5,26 @@ class Paddle {
         this.y = Window.windowHeight/2;
         this.width = 16;
         this.height = Window.windowHeight/6;
-        this.yChange = 0;
+        this.spd = 10
+        this.up = function(){
+            if(this.y < 0){
+                this.spd = 0
+            }else{
+                this.spd = 10
+            }
+            this.y -= this.spd
+            console.log(this.y)
+        }
+        this.down = function(){
+            if(this.y > Window.windowHeight){
+                this.spd = 0
+            }else{
+                this.spd = 10
+            }
+            this.y += this.spd
+            console.log(this.y)
+        }
+        //this.stp = function(){this.y = 0}
 
         if(isLeft){
             this.x = this.width;
